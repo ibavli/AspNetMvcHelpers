125eaf498c99d9c57327071667070723f49f47c3
ibavli <ibrahim.bavli35@gmail.com>
2019-01-23 21:50:01 +0300

    html helpers

---
+++ HTMLHelpers/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ActionLink()
+        {
+            return View();
+        }
+
+        public ActionResult BeginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Gonderdim()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult ParametreliGonderdim(int Id)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Derslerim()
+        {
+            return View();
+        }
+
+        public ActionResult TextBox()
+        {
+            return View();
+        }
+
+        public ActionResult TextArea()
+        {
+            return View();
+        }
+
+        public ActionResult Checkbox()
+        {
+            return View();
+        }
+
+        public ActionResult RadioButton()
+        {
+            return View();
+        }
+
+        public ActionResult DropDownList()
+        {
+            return View();
+        }
+
+        public ActionResult ListBox()
+        {
+            return View();
+        }
+
+        public ActionResult PassWord()
+        {
+            return View();
+        }
+
+        public ActionResult Hidden()
+        {
+            return View();
+        }
+
+        public ActionResult Label()
+        {
+            return View();
+        }
+    }
+}
---
+++ HTMLHelpers/Controllers/MesajIslemleriController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLHelpers.Controllers
+{
+    public class MesajIslemleriController : Controller
+    {
+        public ActionResult MesajGonder()
+        {
+            return View();
+        }
+
+        public ActionResult MesajDetay(int Id)
+        {
+            TempData["Id"] = $"{Id}'li Mesaj";
+            return View();
+        }
+    }
+}
